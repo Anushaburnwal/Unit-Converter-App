a1b6a88d1a3eb0919a5452fae6bf3a5462162498
Anusha Burnwal <anushaburnwal84@gmail.com>
2025-07-07 16:20:49 +0530

    Unit Converter App Done activity_main.xml file

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.unitconverter;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.textview);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
